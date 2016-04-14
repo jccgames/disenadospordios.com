c37d18fab3d64147bd06af876b2c7832ee9ad08f
Juan <jccgamesdrive@gmail.com>
2016-04-13 22:38:25 -0600

    Timeout add

--- public_html/templates/cover/angular.js
+++ public_html/templates/cover/angular.js
@@ -9,6 +9,17 @@ app.directive('cover',function(){
 	});
 
 
-app.controller('sliderController',function($scope){
+app.controller('sliderController',function($scope, $timeout){
 	$scope.slidePos=0;
+
+	var countUp = function() {
+		if ($scope.slidePos > 2) {
+        	$scope.slidePos= 0;
+		} else {
+			$scope.slidePos+=1;
+		}
+        $timeout(countUp, 4000);
+    }
+    
+    $timeout(countUp, 4000);
 });
