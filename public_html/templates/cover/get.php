0779ad09ec3f13d91d4cd46b608ddba9da50e81d
Juan <jccgamesdrive@gmail.com>
2016-04-15 20:51:13 -0600

    array variable

--- public_html/templates/cover/get.php
+++ public_html/templates/cover/get.php
@@ -21,7 +21,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
 // output data of each row
 	while($row = mysqli_fetch_assoc($result)) {
-		echo $row["id"];
+		echo $row;
 	}
 } else {
 	echo "0 results";
