7e03b0ad8cd1e312e1ff13e9cbdc33ba61f106df
Juan <jccgamesdrive@gmail.com>
2016-04-15 22:26:17 -0600

    place echo

--- public_html/templates/cover/prueba.php
+++ public_html/templates/cover/prueba.php
@@ -22,7 +22,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-    	json_encode($row);
+    	echo json_encode($row);
     }
 }
 mysqli_close($conn);
