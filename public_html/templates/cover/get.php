70a74dada21bbf2d524beb935fd6632fc9bd75d1
Juan <jccgamesdrive@gmail.com>
2016-04-15 22:05:21 -0600

    please!

--- public_html/templates/cover/get.php
+++ public_html/templates/cover/get.php
@@ -11,12 +11,11 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+echo "Connection made";
 
 
 $sql = "SELECT id, imagen, titulo, descripcion FROM blogs ORDER BY id DESC LIMIT 4";
 $result = mysqli_query($conn, $sql);
-
-$pos=0;
 
 if (mysqli_num_rows($result) > 0) {
 // output data of each row
