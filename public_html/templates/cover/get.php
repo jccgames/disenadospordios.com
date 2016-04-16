ebc64ff783b91c089b90fcef7559161246d19c3c
Juan <jccgamesdrive@gmail.com>
2016-04-15 21:56:23 -0600

    echo pos

--- public_html/templates/cover/get.php
+++ public_html/templates/cover/get.php
@@ -21,7 +21,7 @@ $pos=0;
 if (mysqli_num_rows($result) > 0) {
 // output data of each row
 	while($row = mysqli_fetch_assoc($result)) {
-		$row['position']=$pos;
+		echo $pos;
 		echo json_encode($row);
 		$pos += 1;
 	}
