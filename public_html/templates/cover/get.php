b4a42f6072210604447745aca7930b2006b8bfc9
Juan <jccgamesdrive@gmail.com>
2016-04-15 21:58:33 -0600

    testing

--- public_html/templates/cover/get.php
+++ public_html/templates/cover/get.php
@@ -13,7 +13,7 @@ if (!$conn) {
 }
 
 
-$sql = "SELECT imagen, titulo, descripcion FROM blogs ORDER BY id DESC LIMIT 4";
+$sql = "SELECT id imagen, titulo, descripcion FROM blogs ORDER BY id DESC LIMIT 4";
 $result = mysqli_query($conn, $sql);
 
 $pos=0;
@@ -21,9 +21,9 @@ $pos=0;
 if (mysqli_num_rows($result) > 0) {
 // output data of each row
 	while($row = mysqli_fetch_assoc($result)) {
-		echo $pos;
+		//echo $pos;
 		echo json_encode($row);
-		$pos += 1;
+		//$pos += 1;
 	}
 } else {
 	echo "0 results";
