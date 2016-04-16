7fb1c202333d49ddefad99c1a6b07f0d8918f104
Juan <jccgamesdrive@gmail.com>
2016-04-15 22:12:53 -0600

    popular

--- public_html/templates/cover/get.php
+++ public_html/templates/cover/get.php
@@ -10,23 +10,22 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-} else 
-{echo "Connection made";}
+}
 
 
-$sql = "SELECT id, imagen, titulo, descripcion FROM blogs ORDER BY id DESC LIMIT 4";
+//We are connected
+
+
+$sql = "SELECT * FROM blogs ORDER BY id DESC LIMIT 4";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-// output data of each row
-	while($row = mysqli_fetch_assoc($result)) {
-		//echo $pos;
-		echo json_encode($row);
-		//$pos += 1;
-	}
-} else {
-	echo "0 results";
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+    	echo json_encode($row);
+    }
 }
-
 mysqli_close($conn);
+
+
 ?>
