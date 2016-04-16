2ac6ebc5b70cb742c30881991ce0f78150f5c483
Juan <jccgamesdrive@gmail.com>
2016-04-15 22:38:33 -0600

    asd

--- public_html/templates/cover/prueba.php
+++ public_html/templates/cover/prueba.php
@@ -18,7 +18,7 @@ if (!$conn) {
 
 $sql = "SELECT id, firstname, lastname, age FROM prueba";
 $result = mysqli_query($conn, $sql);
-
+$data = array()
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
