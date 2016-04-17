7579b4b64d85e8ef02bc9549fe81edb20d6e67df
Juan <jccgamesdrive@gmail.com>
2016-04-16 22:22:33 -0600

    http get slider

--- public_html/app/angular.js
+++ public_html/app/angular.js
@@ -9,34 +9,11 @@ app.directive('cover',function(){
 });
 
 
-app.controller('sliderController',function($scope, $timeout){
+app.controller('sliderController',function($scope, $timeout, $http){
 	$scope.slidePos=0;
-	$scope.slides=[
-		{
-			pos: 0,
-			image: "/images/cover.jpg",
-			title: 'Titulo 0',
-			description: 'Descripcion 0'
-		},
-		{
-			pos: 1,
-			image: "/images/id1.jpg",
-			title: 'Titulo 1',
-			description: 'Descripcion 1'
-		},
-		{
-			pos: 2,
-			image: "/images/id2.jpg",
-			title: 'Titulo 2',
-			description: 'Descripcion 2'
-		},
-		{
-			pos: 3,
-			image: "/images/id3.jpg",
-			title: 'Titulo 3',
-			description: 'Descripcion 3'
-		}
-	]
+	$http.get('/templates/cover/get.php').success(function(data){
+		$scope.slides = data;
+	});
 
 	var countUp = function() {
 		if ($scope.slidePos > 2) {
