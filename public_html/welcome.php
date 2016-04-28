e6f17ba545e391fa4652700e114a114bf469389d
Juan <jccgamesdrive@gmail.com>
2016-04-28 17:22:27 -0600

    stable

--- public_html/welcome.php
+++ public_html/welcome.php
@@ -1,9 +1,8 @@
-
 <html>
 <body>
 
-Welcome <?php echo $_REQUEST["name"]; ?><br>
-Your email address is: <?php echo $_REQUEST["email"]; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
